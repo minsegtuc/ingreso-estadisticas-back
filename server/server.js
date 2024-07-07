c37e3ca8f7ed9e6e385527f07716c3089e684424
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2024-07-07 15:07:10 -0300

    cambio de puerto

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const corsOptions = {
     origin: 'https://inscripciones.minsegtuc.gov.ar',
